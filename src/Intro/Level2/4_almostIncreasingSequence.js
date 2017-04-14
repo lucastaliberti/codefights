448a8dd081f08230d0ddc4c2bb4ef6fde97fb5ed
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-04-14 17:00:16 -0300

    changing to big O notation, had to submit that due to processing time limit, kept the old one for the better style 😁

--- src/Intro/Level2/4_almostIncreasingSequence.js
+++ src/Intro/Level2/4_almostIncreasingSequence.js
@@ -29,12 +29,39 @@ Return true if it is possible to remove one element from the array in order to g
 'use strict'
 const expect = require('chai').expect;
 
-function almostIncreasingSequence(sequence) {
-  return sequence.some((value, index, arr) => {
-    return arr
-      .filter((v, i) => i !== index)
-      .every((v2, i2, arr2) => (i2 > 0) ? v2 > arr2[i2 - 1] : true)
-  })
+// function almostIncreasingSequence(sequence) {
+//   return sequence.some((value, index, arr) => {
+//     return arr
+//       .filter((v, i) => i !== index)
+//       .every((v2, i2, arr2) => (i2 > 0) ? v2 > arr2[i2 - 1] : true)
+//   })
+// }
+
+function almostIncreasingSequence(s) {
+  var ssize = s.length;
+  var i = ssize;
+  var j;
+  while (i--) {
+    var c = true;
+    j = ssize;
+    while (j--) {
+      if (j < i || j > i + 1) {
+        if (s[j] <= s[j - 1]) {
+          c = false;
+          break;
+        }
+      } else if (j == i + 1) {
+        if (s[j] <= s[j - 2]) {
+          c = false;
+          break;
+        }
+      }
+    }
+    if (c) {
+      return true;
+    }
+  }
+  return false;
 }
 
 describe('Intro Level2 - almostIncreasingSequence', function () {
